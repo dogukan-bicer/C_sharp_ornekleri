2a176a3285c77135bee3eaa31cc596afe1de47ed
Doğukan Biçer <dogukanbicer52@gmail.com>
2021-09-30 20:49:01 +0300

    Create toplama_islemi_konsol.cs

---
+++ toplama_islemi_konsol.cs
@@ -0,0 +1,23 @@
+ing System;
+
+namespace testproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Merhaba Dunya!");
+            int sayi1, sayi2, sonuc;
+            Console.WriteLine("Toplama İşlemi Programı");
+            Console.Write("1. Sayi : ");
+            sayi1=Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("2. Sayi : ");
+            sayi2=Convert.ToInt16(Console.ReadLine());
+
+            sonuc=sayi1+sayi2;
+            
+            Console.Write("{0} + {1} = {2}",sayi1, sayi2, sonuc);
+        }
+    }
+}
